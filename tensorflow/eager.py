e94821686ee497caa5bb4f3eed40fcfd01d23081
Zebulun Arendsee <arendsee@iastate.edu>
2018-04-06 14:36:44 -0500

    A little generalization

--- tensorflow/eager.py
+++ tensorflow/eager.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, division, print_function
 import os
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
+from os.path import expanduser
 
 def initialize():
   tf.enable_eager_execution()
@@ -23,17 +24,14 @@ def parse_csv(line):
   return features, label
 
 
-def print_tf():
-  print("TensorFlow version: {}".format(tf.VERSION))
-  print("Eager execution: {}".format(tf.executing_eagerly()))
-
-
 def get_train_dataset():
-  # Download Iris data set to cache directory ($HOME/.keras/datasets/iris_training.csv) 
-  train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
-  # train_dataset_fp is the filepath to the iris dataset
-  train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
-                                             origin=train_dataset_url)
+  train_dataset_fp = expanduser("~/.keras/datasets/iris_training.csv")
+  if(not os.path.exists(train_dataset_fp)):
+    # Download Iris data set to cache directory ($HOME/.keras/datasets/iris_training.csv) 
+    train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
+    # train_dataset_fp is the filepath to the iris dataset
+    train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                               origin=train_dataset_url)
   # set parameters for the training dataset
   train_dataset = tf.data.TextLineDataset(train_dataset_fp)
   train_dataset = train_dataset.skip(1)          # skip the first header row
@@ -43,13 +41,40 @@ def get_train_dataset():
   return train_dataset
 
 
-def make_model():
-  model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),  # input shape required
-    tf.keras.layers.Dense(10, activation="relu"),
-    tf.keras.layers.Dense(3)
-  ])
-  return model
+def get_test_dataset():
+  test_fp = expanduser("~/.keras/datasets/iris_test.csv")
+  if(not os.path.exists(test_fp)):
+    test_url = "http://download.tensorflow.org/data/iris_test.csv"
+    test_fp = tf.keras.utils.get_file(
+      fname=os.path.basename(test_url),
+      origin=test_url
+    )
+
+  test_dataset = tf.data.TextLineDataset(test_fp)
+  test_dataset = test_dataset.skip(1)             # skip header row
+  test_dataset = test_dataset.map(parse_csv)      # parse each row with the funcition created earlier
+  test_dataset = test_dataset.shuffle(1000)       # randomize
+  test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
+
+  return test_dataset
+
+
+def make_model(depth, bounds):
+  layers = [
+    tf.keras.layers.Dense(
+      depth[0], # number of nodes in first layer
+      activation="relu",
+      input_shape=(bounds[0],)
+    )
+  ] + [
+    tf.keras.layers.Dense(
+       d, # number of nodes in ith layer
+       activation="relu"
+    ) for d in depth[1:-1]
+  ] + [
+    tf.keras.layers.Dense(bounds[1])
+  ]
+  return tf.keras.Sequential(layers)
 
 
 def make_loss():
@@ -67,9 +92,9 @@ def make_grad(loss):
   return grad
 
 
-def make_optimizer():
+def make_optimizer(learning_rate=0.01):
   # Choose and parameterize the optimizing algorithm
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
   return optimizer
 
 
@@ -87,23 +112,6 @@ def train_model(train_dataset, model, grad, optimizer, num_epochs=201):
   return model 
 
 
-def get_test_dataset():
-  test_url = "http://download.tensorflow.org/data/iris_test.csv"
-
-  test_fp = tf.keras.utils.get_file(
-    fname=os.path.basename(test_url),
-    origin=test_url
-  )
-
-  test_dataset = tf.data.TextLineDataset(test_fp)
-  test_dataset = test_dataset.skip(1)             # skip header row
-  test_dataset = test_dataset.map(parse_csv)      # parse each row with the funcition created earlier
-  test_dataset = test_dataset.shuffle(1000)       # randomize
-  test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
-
-  return test_dataset
-
-
 def test_model(test_dataset, model):
   test_accuracy = tfe.metrics.Accuracy()
   for (x, y) in tfe.Iterator(test_dataset):
@@ -112,37 +120,40 @@ def test_model(test_dataset, model):
   return test_accuracy
 
 
-def make_example():
-  return (
-    tf.convert_to_tensor([
-      [5.1, 3.3, 1.7, 0.5,],
-      [5.9, 3.0, 4.2, 1.5,],
-      [6.9, 3.1, 5.4, 2.1]
-    ]),
-    ["Iris setosa", "Iris versicolor", "Iris virginica"]
-  )
-
-
 def print_prediction(predictions, class_ids):
-  """  print_prediction(model(make_example())) """
   for i, logits in enumerate(predictions):
     class_idx = tf.argmax(logits).numpy()
     name = class_ids[class_idx]
-    print("Example {} prediction: {}".format(i, name))
+    print("{}\t{}".format(i, name))
 
 
 if __name__ == '__main__':
   initialize()
   train_d = get_train_dataset()
   test_d = get_test_dataset()
+
+  # These are the three main 
+  depth = [10,10]
+  num_epochs = 1000
+
+  model = make_model(depth, bounds=(4,3))
+
   model = train_model(
     train_dataset = train_d,
-    model         = make_model(),
+    model         = model,
     grad          = make_grad(make_loss()),
     optimizer     = make_optimizer(),
-    num_epochs    = 201
+    num_epochs    = num_epochs
   )
   print("Test set accuracy: {:.3%}".format(test_model(test_d, model).result()))
-  example_d = make_example()
-  pred = model(example_d[0])
-  print_prediction(pred, example_d[1])
+
+
+  example_d = tf.convert_to_tensor([
+      [5.1, 3.3, 1.7, 0.5,],
+      [5.9, 3.0, 4.2, 1.5,],
+      [6.9, 3.1, 5.4, 2.1]
+    ])
+  class_ids = ["Iris setosa", "Iris versicolor", "Iris virginica"]
+
+  pred = model(example_d)
+  print_prediction(pred, class_ids)
